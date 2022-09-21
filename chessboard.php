3f97d1ee931795e58be78bc58fe8dea5f397763f
charviis <marekgta233@gmail.com>
2022-09-21 16:57:50 +0200

    done

--- chessboard.php
+++ chessboard.php
@@ -1,36 +1,38 @@
 <!DOCTYPE html>
 <html>
+<head>
+    <title>Chessboard</title>
+    <style>
+        table {
+            border-collapse: collapse;
+        }
+        td {
+            width: 50px;
+            height: 50px;
+        }
+        .black {
+            background-color: black;
+        }
+    </style>
 
 <body>
 	<table width="400px" border="1px" cellspacing="0px">
 		<?php
-		echo "Chess by GeeksforGeeks";
-		$value = 0;
-		$col = 0;
-
-		do {
-			$row = 0;
-			echo "<tr>";
-			$value = $col;
-
-			do {
-				if($value%2 == 0) {
-					echo
-"<td height=40px width=20px bgcolor=black></td>";
-					$value++;
-				}
-				else {
-					echo
-"<td height=40px width=20px bgcolor=white></td>";
-					$value++;
-				}
-				$row++;
-			}while($row < 8);
-			echo "</tr>";
-			$col++;
-		}while($col < 8);
-		?>
+            for($i=1; $i<=8; $i++) {
+                echo "<tr>";
+                for($j=1; $j<=8; $j++) {
+                    $total = $i + $j;
+                    if($total % 2 == 0) {
+                        echo "<td class='black'></td>";
+                    } else {
+                        echo "<td></td>";
+                    }
+                }
+                echo "</tr>";
+            }
+        ?>
 	</table>
 </body>
 
 </html>
+
